5500b04db35d9f9c778c6b661f6c032267aff47d
Lishashajis <156441856+Lishashajis@users.noreply.github.com>
2024-01-15 21:23:46 +0530

    Create index.php
    Demo

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <?php
+echo '<p style="color: blue;">This is a passage with HTML formatting and color.</p>';
+echo '<p style="color: blue;">This is a Laravel Project</p>';
+echo '<p style="color: green;">This is a Project</p>';
+echo '<p style="color: green;">This is a  line</p>';
+
+?>
+</html>
